2a4461ac87348ab2c0b289a0d2a4cb1df8a1c537
gerardoMarroquin09 <ma19074@ues.edu.sv>
2025-04-01 16:42:48 -0600

    Creando los endpoints para productos

---
+++ app-pruebas/server/utils/mongo/productos.ts
@@ -0,0 +1,110 @@
+import pruebas from '../pruebas.json';
+
+async function productosInsertar(total: number): Promise<number> { 
+    console.log("Iniciando inserción de productos en MongoDB");   
+    let start = new Date().getTime();    
+    for (let i = 1; i <= total; i++) {
+        const ldata = {
+            id: i,
+            nombre: "Producto " + i,
+            precio: Math.floor(Math.random() * 100) + 1,
+            idCategoria: Math.floor(Math.random() * pruebas.categorias.insertar) + 1,
+        }
+        // Agrego usando $fetch        
+        await $fetch('http://localhost:3000/api/mongo/producto', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(ldata),            
+            onRequestError({ request, options, error }) {
+                return -1;
+            },
+        })        
+    }
+    let end = new Date().getTime();
+    let time = end - start;
+    return time;
+}
+
+async function productosConsultar(): Promise<number>  {    
+    let start = new Date().getTime();
+    await $fetch('http://localhost:3000/api/mongo/productos', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        onRequestError({ request, options, error }) {
+            return -1
+        },
+    })
+    let end = new Date().getTime();
+    let time = end - start;
+    return time;       
+}
+
+async function productosConsultarAzar(total:number): Promise<number>  {    
+    let start = new Date().getTime();
+    for (let i = 1; i <= total; i++) {
+        let id = Math.floor(Math.random() * pruebas.productos.insertar) + 1;
+        await $fetch('http://localhost:3000/api/mongo/producto/' + id, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            onRequestError({ request, options, error }) {
+                return -1;
+            },
+        })        
+    }
+    let end = new Date().getTime();
+    let time = end - start;
+    return time; 
+}
+
+async function productosActualizar(total:number): Promise<number>  {
+    let start = new Date().getTime();
+    for (let i = 1; i <= total; i++) {
+        const ldata = {
+            id: i,
+            nombre: "Producto " + i + " Actualizado",
+            precio: Math.floor(Math.random() * 100) + 1,
+            idCategoria: Math.floor(Math.random() * pruebas.categorias.insertar) + 1,
+        }
+
+        await $fetch('http://localhost:3000/api/mongo/producto/'+i, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(ldata),
+            onRequestError({ request, options, error }) {
+                return -1;
+            },
+        })        
+    }
+    let end = new Date().getTime();
+    let time = end - start;
+    return time;
+}
+
+async function productosEliminar(total:number): Promise<number>  {
+    let start = new Date().getTime();
+    for (let i = 1; i <= total; i++) {
+        await $fetch('http://localhost:3000/api/mongo/producto/'+i, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ id: i }),
+        onRequestError({ request, options, error }) {
+            return -1;
+        }
+        })  
+    }
+    let end = new Date().getTime();
+    let time = end - start;
+    return time; 
+}
+
+export { productosInsertar, productosConsultar, productosConsultarAzar, productosActualizar, productosEliminar };
