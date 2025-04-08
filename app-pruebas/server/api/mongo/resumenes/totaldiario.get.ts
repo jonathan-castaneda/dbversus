ab62a49345460c911440881b0702631f17e49d36
gerardoMarroquin09 <ma19074@ues.edu.sv>
2025-04-07 19:54:56 -0600

    Correcion en Resumenes
    Correcion en endpoint de totaldiario.get.ts

--- app-pruebas/server/api/mongo/resumenes/totaldiario.get.ts
+++ app-pruebas/server/api/mongo/resumenes/totaldiario.get.ts
@@ -1,37 +1,34 @@
-import { Orden } from "../../../utils/mongo/mongo"; 
+import { Orden } from "../../../utils/mongo/mongo";
 
 export default defineEventHandler(async (event) => {
   try {
     const data = await Orden.aggregate([
-      { $unwind: "$detalleOrden" },
       {
         $group: {
-          _id: "$detalleOrden.nombre", // Agrupamos por el nombre del producto
-          cantidad: { $sum: "$detalleOrden.cantidad" }, // Sumamos las cantidades de cada producto
+          _id: {
+            $dateToString: { format: "%Y-%m-%d", date: "$fecha" }
+          },
+          total: { $sum: "$total" }
         }
       },
       {
-        $sort: { cantidad: -1 } // Ordenamos por cantidad de manera descendente
-      },
-      {
-        $limit: 10 // Limitamos a los 10 primeros productos más vendidos
+        $sort: { _id: 1 }
       },
       {
         $project: {
-          nombre: "$_id", // Asignamos el valor de _id a "nombre"
-          cantidad: 1, // Devolvemos la cantidad
-          _id: 0 // Excluimos el campo _id
+          fecha: "$_id",
+          total: 1,
+          _id: 0
         }
       }
     ]);
 
-    // Retornamos los datos con un estado exitoso
     return { statusCode: 200, data };
-  } catch (error) {
-    console.error("Error al obtener el total diario", error);
+  } catch (error: any) {
+    console.error("Error total diario:", error);
     return {
       statusCode: 500,
-      error: (error as Error).message, // Retornamos el mensaje de error
+      error: error.message
     };
   }
 });
