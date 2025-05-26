4905f27287d3225095c5168590b0cd7439caf732
eliasronquillo00 <elias.ronquillo00@gmail.com>
2025-05-26 11:37:12 -0600

    endpoints actualizados y resumenes

---
+++ app-pruebas/server/api/rethinkdb/productos/consultar.ts
@@ -0,0 +1,42 @@
+// /server/api/rethinkdb/productos/insertar.ts
+import { connect, rethink } from '~/server/utils/rethinkdb/rethinkdb';
+import { defineEventHandler, readBody, createError } from 'h3' // desde la raiz del proyecto 
+
+export default defineEventHandler(async (event) => {
+  try {
+
+
+    // verificamos la conexion
+    const conn = await connect();
+    if (!conn) {
+      throw createError({
+        statusCode: 500,
+        statusMessage: 'No se pudo conectar a la base de datos.',
+      });
+    }
+
+    // obtenemos todos los ids 
+    const cursor = await rethink.table('productos').pluck('id').run(conn);
+    const docs = await cursor.toArray();
+    const ids = docs.map(doc => doc.id);
+
+    const resultados = [];
+
+    for (const id of ids) {
+      const doc = await rethink.table('productos').get(id).run(conn); // consultamos documento x documento 
+      resultados.push(doc);
+    }
+
+    return { success: true, resultados };
+
+
+
+  } catch (error) {
+    console.error('Error al obtener las productos:', error);
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Error al obtener las productos.',
+    });
+  }
+});
+
